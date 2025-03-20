2e49aa6faec8e67bf72a3598aaac242d848b8e9f
Perry Sahnow <57735040+perrys25@users.noreply.github.com>
2025-03-19 22:54:58 -0400

    Add Methods to Select

--- src/main/java/me/perrysahnow/reefselector/widget/PositionSelector.java
+++ src/main/java/me/perrysahnow/reefselector/widget/PositionSelector.java
@@ -7,9 +7,11 @@ import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 
 import java.io.InputStream;
+import java.util.HashMap;
 
 @Description(name = "PositionSelector", summary = "Shows a different image of the selected position based on a value", dataTypes = String.class)
 @ParametrizedController("PositionSelector.fxml")
@@ -21,19 +23,73 @@ public class PositionSelector extends SimpleAnnotatedWidget<String> {
     @FXML
     private ImageView reefPositions;
 
+    private HashMap<String, Image> reefImages = new HashMap<>();
+
     @FXML
     private void initialize() {
+
+        for (char c = 'A'; c <= 'L'; c++) {
+            String letter = String.valueOf(c);
+            reefImages.put(letter, new Image(getClass().getResourceAsStream("images/Reef_Reef Position " + letter + ".png")));
+        }
+        reefImages.put("Empty", new Image(getClass().getResourceAsStream("images/Reef_Reef Positions Empty.png")));
+
         reefPositions.imageProperty().bind(Bindings.createObjectBinding(() -> {
             String data = getData();
             if (data != null && !data.equals("")) {
-                String pose = getData();
-                InputStream steam = getClass().getResourceAsStream("images/Reef_Reef Position " + pose + ".png");
-                if (steam != null) {
-                    return new Image(steam);
+                if (reefImages.containsKey(data)) {
+                    return reefImages.get(data);
                 }
             }
-            return new Image(getClass().getResourceAsStream("images/Reef_Reef Positions Empty.png"));
+            return reefImages.get("Empty");
         }, dataProperty()));
+
+        reefPositions.setOnMouseClicked(event -> {
+            String data = getData();
+            if (data != null && !data.equals("")) {
+                char letter = data.charAt(0);
+                if (!event.isShiftDown()) {
+                    if (letter == 'L') {
+                        setData("A");
+                    } else {
+                        setData(String.valueOf((char) (letter + 1)));
+                    }
+                } else {
+                    if (letter == 'A') {
+                        setData("L");
+                    } else {
+                        setData(String.valueOf((char) (letter - 1)));
+                    }
+                }
+            } else {
+                setData("A");
+            }
+        });
+
+        reefPositions.setOnKeyPressed(event -> {
+            String data = getData();
+            char character = event.getCharacter().charAt(0);
+            if (character >= 'A' && character <= 'L') {
+                setData(String.valueOf(character));
+            } else {
+                if (data != null && !data.equals("")) {
+                    char letter = data.charAt(0);
+                    if (event.getCode() == KeyCode.LEFT) {
+                        if (letter == 'L') {
+                            setData("A");
+                        } else {
+                            setData(String.valueOf((char) (letter + 1)));
+                        }
+                    } else if (event.getCode() == KeyCode.RIGHT) {
+                        if (letter == 'A') {
+                            setData("L");
+                        } else {
+                            setData(String.valueOf((char) (letter - 1)));
+                        }
+                    }
+                }
+            }
+        });
     }
 
     @Override
--- src/main/java/me/perrysahnow/reefselector/widget/ReefSelector.java
+++ src/main/java/me/perrysahnow/reefselector/widget/ReefSelector.java
@@ -7,9 +7,11 @@ import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 
 import java.io.InputStream;
+import java.util.HashMap;
 
 
 @Description(name = "ReefSelector", summary = "Shows a different image of the reef based on a given value", dataTypes = String.class)
@@ -22,37 +24,80 @@ public class ReefSelector extends SimpleAnnotatedWidget<String> {
     @FXML
     private ImageView coralReef;
 
+    private HashMap<String, Image> reefImages = new HashMap<>();
+
     @FXML
     private void initialize() {
+        reefImages.put("L2", new Image(getClass().getResourceAsStream("images/Reef_L2 Reef.png")));
+        reefImages.put("L3", new Image(getClass().getResourceAsStream("images/Reef_L3 Reef.png")));
+        reefImages.put("L4", new Image(getClass().getResourceAsStream("images/Reef_L4 Reef.png")));
+        reefImages.put("Empty", new Image(getClass().getResourceAsStream("images/Reef_Empty Reef.png")));
+
         coralReef.imageProperty().bind(Bindings.createObjectBinding(() -> {
             String data = getData();
             if (data != null && !data.equals("")) {
-                String reefLevel = getData();
-                InputStream steam = getClass().getResourceAsStream("images/Reef_" + reefLevel + " Reef.png");
-                if (steam != null) {
-                    return new Image(getClass().getResourceAsStream("images/Reef_" + reefLevel + " Reef.png"));
+                if (reefImages.containsKey(data)) {
+                    return reefImages.get(data);
                 }
             }
-            return new Image(getClass().getResourceAsStream("images/Reef_Empty Reef.png"));
+            return reefImages.get("Empty");
         }, dataProperty()));
         coralReef.setOnMouseClicked(event -> {
             String data = getData();
             if (data != null && !data.equals("")) {
-                switch (data) {
-                    case "L2":
-                        setData("L3");
-                        break;
-                    case "L3":
-                        setData("L4");
-                        break;
-                    default:
-                        setData("L2");
-                        break;
+                if (!event.isShiftDown()) {
+                    raiseLevel(data);
+                } else {
+                    lowerLevel(data);
                 }
             } else {
                 setData("L4");
             }
         });
+
+        coralReef.setOnKeyPressed(event -> {
+            String data = getData();
+            char character = event.getCharacter().charAt(0);
+            if (character >= '2' && character <= '4') {
+                setData("L" + character);
+            } else {
+                if (data != null && !data.equals("")) {
+                    if (event.getCode() == KeyCode.UP) {
+                        raiseLevel(data);
+                    } else if (event.getCode() == KeyCode.DOWN) {
+                        lowerLevel(data);
+                    }
+                }
+            }
+        });
+    }
+
+    private void lowerLevel(String data) {
+        switch (data) {
+            case "L2":
+                setData("L4");
+                break;
+            case "L3":
+                setData("L2");
+                break;
+            default:
+                setData("L3");
+                break;
+        }
+    }
+
+    private void raiseLevel(String data) {
+        switch (data) {
+            case "L2":
+                setData("L3");
+                break;
+            case "L4":
+                setData("L2");
+                break;
+            default:
+                setData("L4");
+                break;
+        }
     }
 
     @Override
